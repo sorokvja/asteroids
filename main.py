c216958e27a9aefdfbbcd93d471aa8a563cb3782
sorokvja <vjacheslav.sorokin@gmail.com>
2025-04-16 09:35:40 +0300

    add draw player

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import * 
+from player import Player 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!") 
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,8 +23,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.draw(screen) 
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main() 
